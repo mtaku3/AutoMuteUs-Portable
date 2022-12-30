cf253045cd34221b2bddc181ed96c1da2e734393
mtaku3 <m.taku3.1222@gmail.com>
2022-12-30 22:41:35 +0900

    fix: fix ZipFileProgressExtensions

---
+++ AutoMuteUs Portable.Shared/Utility/ZipFileProgressExtensions/PathInternal.cs
@@ -0,0 +1,22 @@
+﻿namespace AutoMuteUs_Portable.Shared.Utility.ZipFileProgressExtensionsNS;
+
+internal static class PathInternal
+{
+    /// <summary>Returns a comparison that can be used to compare file and directory names for equality.</summary>
+    internal static StringComparison StringComparison =>
+        IsCaseSensitive ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
+
+    /// <summary>Gets whether the system is case-sensitive.</summary>
+    internal static bool IsCaseSensitive
+    {
+        get
+        {
+#if MS_IO_REDIST
+                return false; // Windows is always case-insensitive
+#else
+            return !(OperatingSystem.IsWindows() || OperatingSystem.IsMacOS() || OperatingSystem.IsIOS() ||
+                     OperatingSystem.IsTvOS() || OperatingSystem.IsWatchOS());
+#endif
+        }
+    }
+}
