35449e860f7db06f94e9f86231f43c6e923ba87a
mtaku3 <m.taku3.1222@gmail.com>
2023-01-13 16:47:24 +0900

    feat: add progress to executor method and make it async

--- AutoMuteUs Portable.Shared/Controller/Executor/IExecutorController.cs
+++ AutoMuteUs Portable.Shared/Controller/Executor/IExecutorController.cs
@@ -7,15 +7,15 @@ public interface IExecutorController
 {
     public static Dictionary<string, Parameter> InstallParameters;
     public static Dictionary<string, Parameter> UpdateParameters;
-    public void Run();
-    public void Stop();
-    public void Restart();
-    public Task Install(Dictionary<string, string> parameters, ISubject<ProgressInfo> progress);
-    public Task Update(Dictionary<string, string> parameters, ISubject<ProgressInfo> progress);
+    public Task Run(ISubject<ProgressInfo>? progress = null);
+    public Task Stop(ISubject<ProgressInfo>? progress = null);
+    public Task Restart(ISubject<ProgressInfo>? progress = null);
+    public Task Install(Dictionary<string, string> parameters, ISubject<ProgressInfo>? progress = null);
+    public Task Update(Dictionary<string, string> parameters, ISubject<ProgressInfo>? progress = null);
 
     public Task InstallBySimpleSettings(dynamic simpleSettings, dynamic executorConfigurationBase,
-        ISubject<ProgressInfo> progress);
+        ISubject<ProgressInfo>? progress = null);
 
     public Task UpdateBySimpleSettings(dynamic simpleSettings, dynamic executorConfigurationBase,
-        ISubject<ProgressInfo> progress);
+        ISubject<ProgressInfo>? progress = null);
 }
