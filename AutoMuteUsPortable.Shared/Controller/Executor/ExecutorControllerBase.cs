6a6f4a2f703bd59ff0597248e87744b1aec02323
mtaku3 <m.taku3.1222@gmail.com>
2023-01-14 11:27:59 +0900

    feat: change ExecutorController from interface to base class

---
+++ AutoMuteUsPortable.Shared/Controller/Executor/ExecutorControllerBase.cs
@@ -0,0 +1,66 @@
+﻿using System.Reactive.Subjects;
+using AutoMuteUsPortable.Shared.Entity.ProgressInfo;
+
+namespace AutoMuteUsPortable.Shared.Controller.Executor;
+
+public class ExecutorControllerBase
+{
+    public static Dictionary<string, Parameter> InstallParameters = new();
+    public static Dictionary<string, Parameter> UpdateParameters = new();
+
+    public ExecutorControllerBase(dynamic executorConfiguration)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ExecutorControllerBase(dynamic computedSimpleSettings,
+        dynamic executorConfigurationBase)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsRunning { get; protected set; }
+    public event EventHandler Stopped;
+
+    public virtual void OnStop()
+    {
+        Stopped?.Invoke(this, EventArgs.Empty);
+    }
+
+    public virtual Task Run(ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task Stop(ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task Restart(ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task Install(Dictionary<string, string> parameters, ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task Update(Dictionary<string, string> parameters, ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task InstallBySimpleSettings(dynamic simpleSettings, dynamic executorConfigurationBase,
+        ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual Task UpdateBySimpleSettings(dynamic simpleSettings, dynamic executorConfigurationBase,
+        ISubject<ProgressInfo>? progress = null)
+    {
+        throw new NotImplementedException();
+    }
+}
